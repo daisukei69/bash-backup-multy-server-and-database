54839d5215482ea5a7546aea41a8515701d27148
Adityo N <daisukei.69@gmail.com>
2021-08-07 12:39:59 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -23,6 +23,10 @@ do
 	DEST=/mnt/backup/${Get_db}
 	
 	FILEBACKUP="${DEST}/${Get_db}_$CURRDATE.sql"
+	
+	FILEBACKUP_STRUCTURE="${DEST}/${Get_db}_$CURRDATE_STRUCTURE_ONLY.sql"
+	
+	FILEBACKUP_DATA="${DEST}/${Get_db}_$CURRDATE_DATA_ONLY.sql"
 		
 	#if empty folder destination then create folder destination
 	if [ ! -d "$DEST" ]; then
@@ -31,6 +35,12 @@ do
 	
 	#backup Data Base use mysqldump
 	mysqldump --lock-tables=false --single-transaction -u$Get_user -h$Get_host -p$Get_pass -P$Get_port $Get_db > $FILEBACKUP
+	
+	#backup Data Only use mysqldump
+	mysqldump --lock-tables=false --single-transaction -u$Get_user -h$Get_host -p$Get_pass -P$Get_port --no-create-info $Get_db > $FILEBACKUP_DATA
+	
+	#backup Data Only use mysqldump
+	mysqldump --lock-tables=false --single-transaction -u$Get_user -h$Get_host -p$Get_pass -P$Get_port --no-data $Get_db > $FILEBACKUP_STRUCTURE
 	
 	#archive file sql to tar.xz
 	tar -cjf ${DEST}/${Get_db}_$CURRDATE.tar.xz --absolute-names --directory=${DEST}/ ${Get_db}_$CURRDATE.sql
